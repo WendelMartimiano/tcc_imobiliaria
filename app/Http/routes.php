52498fce9303c837a3f74adc8835bd0dd4f8948a
Wendel Martimiano Gonçalves <wendelprogrammer@gmail.com>
2016-08-19 00:38:23 -0300

    Iniciando a implementacao do login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+//Grupo de rota do site
 Route::group(['prefix' => 'site'], function () {
 
    // Route::get('teste/{mensagem}', 'Site\UsuarioController@postMailConfirmacao');
@@ -34,6 +20,23 @@ Route::group(['prefix' => 'site'], function () {
     Route::controller('/', 'Site\PlanoController');
 });
 
+// Rotas de autenticação
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Rotas de registro de usuários
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+//Rota do dashboard da imobweb
+Route::get('/dashboard', function () {
+    return view('imobweb.dashboard');
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 /*
  Teste de submissão de formulario
---
+++ resources/views/imobweb/dashboard.blade.php
@@ -0,0 +1,1 @@
+<p>Bem Vindo a ImobWeb!</p>
